a94449230728912664f290d30ad69c9c058d883e
Whitt <cwhittle@zweiggroup.com>
2017-08-09 10:07:13 -0500

    if I set the password everytime it would log people out... This checks to see if it's the current password and only resets it if it has changed

--- lib/Auth0Service.php
+++ lib/Auth0Service.php
@@ -78,77 +78,86 @@ class Auth0Service
 
     public function authenticate( $user, $user_email, $password )
     {
-        if (is_a($user, 'WP_User') ) {
+        if ($user instanceof WP_User ) {
             return $user;
         }
+
         if (empty($user_email) || empty($password) ) {
             if (is_wp_error($user) ) {
                 return $user;
             }
-            return false;
+            $error = new WP_Error();
+            if (empty($user_email) ) {
+                $error->add('empty_username', __('<strong>ERROR</strong>: The email field is empty.')); // Uses 'empty_username' for back-compat with wp_signon()
+            }
+            if (empty($password) ) {
+                $error->add('empty_password', __('<strong>ERROR</strong>: The password field is empty.'));
+            }
+            return $error;
         }
 
-        if (!empty($user_email) && is_email($user_email) ) {
-            $authentication = $this->createAuthentication();
-            if($user_email) {
-                $options = array("username"=>$user_email,"password"=>$password,"realm"=>$this->connection,"scope"=>$this->scope);
-                $result = null;
+        if (!is_email($user_email) ) {
+            return $user;
+        }
+
+        $authentication = $this->createAuthentication();
+        if($user_email) {
+            $options = array("username"=>$user_email,"password"=>$password,"realm"=>$this->connection,"scope"=>$this->scope);
+            $result = null;
+            try {
+                $result = (object) $authentication->login($options);
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+            }
+
+            if(!$result) {
+                $error = new WP_Error();
+                $error->add("unknown", __("<strong>ERROR</strong>: I'm sorry there was an unknown error, please try again and if it continues, please contact ".$this->support_email.".  Thank you"));
+                return $error;
+            }else if(isset($result->error)) {
+                $error = new WP_Error();
+                $error->add($result->error, __('<strong>ERROR</strong>: ' . $result->error_description));
+                return $error;
+            }else{
                 try {
-                    $result = (object) $authentication->login($options);
+                    $user_profile = JWT::decode($result->id_token, $this->client_secret, array('HS256', 'RS256'));
+                    if(!$user_profile->email_verified) {
+                        $error = new WP_Error();
+                        $error->add("not_validation", __('<strong>ERROR</strong>: Check your inbox for an verfication email, once verfied you can login.'));
+                        return $error;
+                    }
+                    $is_new = false;
+                    $user = get_user_by('email', $user_email);
+                    if($user) {
+                        $user_id = $user->ID;
+                    }else{
+                        $user_id = $this->getUserIDFromAuth0ID($user_profile->sub);
+                        if(!$user_id) {
+                            $is_new = true;
+                            $user_id = wp_create_user($user_email, $password, $user_email);
+                        }
+                    }
+
+                    wp_update_user(array( 'ID' => $user_id, 'display_name' => $user_profile->nickname, "user_email" => $user_profile->email));
+                    global $wpdb;
+                    update_user_meta($user_id, $wpdb->prefix.'auth0_id', ( isset($user_profile->user_id) ? $user_profile->user_id : $user_profile->sub ));
+                    update_user_meta($user_id, $wpdb->prefix.'auth0_obj', json_encode($user_profile));
+                    update_user_meta($user_id, $wpdb->prefix.'last_update', date('c'));
+                    
+                    if(!wp_check_password($password, $user->data->user_pass, $user_id)) {
+                        wp_set_password($password, $user_id);
+                    }
+
+                    do_action('auth0_user_login', $user_id, $user_profile, $is_new, $result->id_token, $result->access_token);
+                    return $user;
                 } catch (Exception $e) {
                     error_log($e->getMessage());
-                }
-
-                if(!$result) {
                     $error = new WP_Error();
                     $error->add("unknown", __("<strong>ERROR</strong>: I'm sorry there was an unknown error, please try again and if it continues, please contact ".$this->support_email.".  Thank you"));
                     return $error;
-                }else if(isset($result->error)) {
-                    $error = new WP_Error();
-                    $error->add($result->error, __('<strong>ERROR</strong>: ' . $result->error_description));
-                    return $error;
-                }else{
-                    try {
-                        $user_profile = JWT::decode($result->id_token, $this->client_secret, array('HS256', 'RS256'));
-                        if(!$user_profile->email_verified) {
-                            $error = new WP_Error();
-                            $error->add("not_validation", __('<strong>ERROR</strong>: Check your inbox for an verfication email, once verfied you can login.'));
-                            return $error;
-                        }
-                        $is_new = false;
-                        $user = get_user_by('email', $user_email);
-                        if($user) {
-                            $user_id = $user->ID;
-                        }else{
-                            $user_id = $this->getUserIDFromAuth0ID($user_profile->sub);
-                            if(!$user_id) {
-                                $is_new = true;
-                                $user_id = wp_create_user($user_email, $password, $user_email);
-                            }
-                        }
-
-                        wp_update_user(array( 'ID' => $user_id, 'display_name' => $user_profile->nickname, "user_email" => $user_profile->email));
-                        global $wpdb;
-                        update_user_meta($user_id, $wpdb->prefix.'auth0_id', ( isset($user_profile->user_id) ? $user_profile->user_id : $user_profile->sub ));
-                        update_user_meta($user_id, $wpdb->prefix.'auth0_obj', json_encode($user_profile));
-                        update_user_meta($user_id, $wpdb->prefix.'last_update', date('c'));
-
-                        wp_set_password($password, $user_id);
-
-                        do_action('auth0_user_login', $user_id, $user_profile, $is_new, $result->id_token, $result->access_token);
-                        return $user;
-                    } catch (Exception $e) {
-                        error_log($e->getMessage());
-                        $error = new WP_Error();
-                        $error->add("unknown", __("<strong>ERROR</strong>: I'm sorry there was an unknown error, please try again and if it continues, please contact ".$this->support_email.".  Thank you"));
-                        return $error;
-                    }
                 }
-                //Should never get here but if it does....
-                return false;
             }
-        }
-        if (!empty($user_email) || !empty($password) ) {
+            //Should never get here but if it does....
             return false;
         }
     }
--- lib/AuthenticationOverride.php
+++ lib/AuthenticationOverride.php
@@ -15,17 +15,13 @@ class AuthenticationOverride
                     remove_filter('authenticate', 'wp_authenticate_email_password', 20, 3);
                     add_filter(
                         'authenticate', function ( $user, $user_email, $password ) {
-                            if($user_email && $password) {
-                                return $this->auth0_service->authenticate($user, $user_email, $password);
-                            }else{
-                                return false;
-                            }
+                            return $this->auth0_service->authenticate($user, $user_email, $password);
                         }, 20, 3
                     );
                 }
             }
         );
-        
+
         add_action(
             "login_form", function () {
                 if(isset($_GET["wle"])) {
